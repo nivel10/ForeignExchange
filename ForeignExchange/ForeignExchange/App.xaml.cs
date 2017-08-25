775628bf28a6fd9ff4cf92ba4b83beb9915b49d9
Carlos H <carlos.e.herrera.j@hotmail.com>
2017-08-25 00:06:14 -0400

    Finished Desing

---
+++ ForeignExchange/ForeignExchange/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace ForeignExchange
+{
+    using ForeignExchange.Views;
+    using Xamarin.Forms;
+
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //  MainPage = new ForeignExchange.MainPage();
+            MainPage = new MainView();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
